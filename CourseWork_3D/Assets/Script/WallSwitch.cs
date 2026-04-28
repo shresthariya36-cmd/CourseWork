0c8d7aaf0283904214e01c59d3b80c6629e92a2a
shresthariya36-cmd <shresthariya36@gmail.com>
2026-04-28 11:17:17 +0545

    lighting working

--- CourseWork_3D/Assets/Script/WallSwitch.cs
+++ CourseWork_3D/Assets/Script/WallSwitch.cs
@@ -2,24 +2,31 @@ using UnityEngine;
 
 public class WallSwitch : MonoBehaviour
 {
-    public Transform player;
-    public Light roomLight;
+    public Light myLight;
 
-    public float interactRange = 2.25f;
+    private bool playerInRange = false;
 
     void Update()
     {
-        float distance = Vector3.Distance(transform.position, player.position);
-
-        if (distance <= interactRange)
+        if (playerInRange && Input.GetKeyDown(KeyCode.E))
         {
-            if (Input.GetKeyDown(KeyCode.E))
-            {
-                if (roomLight != null)
-                {
-                    roomLight.enabled = !roomLight.enabled;
-                }
-            }
+            myLight.enabled = !myLight.enabled;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
         }
     }
 }
